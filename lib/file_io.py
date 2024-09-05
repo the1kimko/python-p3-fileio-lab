a04d7640e9669f1e3001c9ab5d91db2e226f8aae
KomfortKimko <komfortkimk@gmail.com>
2024-09-05 17:35:18 +0300

    fileio-lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,32 @@
+import os
+
 def write_file(file_name, file_content):
-    pass
+    """
+    Creates of overwrites a .txt file with the provided file content.
+    """
+    full_file_name = f"{file_name}.txt"
+
+    with open(full_file_name, mode="w", encoding="utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Appends provided content to a .txt file.
+    """
+    full_file_name = f"{file_name}.txt"
+
+    with open(full_file_name, mode="a", encoding="utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """
+    Reads and returns content of the .tx file.
+    """
+    full_file_name = f"{file_name}.txt"
+
+    # Read the content from the file
+    if os.path.exists(full_file_name):
+        with open(full_file_name, mode="r", encoding="utf-8") as file:
+            return file.read()
+    else:
+        return "File does not exist."
